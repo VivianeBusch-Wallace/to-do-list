1c9c4e92efa0f30c0bf0a4ce0e392931b7d4cfd6
viviane <v.buschwallace@yahoo.com>
2021-04-26 12:11:24 +0200

    Updates

---
+++ scripts/index.js
@@ -0,0 +1,20 @@
+const addItem = () => {
+  // grab value of input >>
+  let listItem = document.querySelector("#new-to-do-item").value;
+  //if input is empty don't add to to do list >>
+  if (listItem != "") {
+    // create new list item for ul >>
+    let newLi = document.createElement("li");
+    // define text, create node from listItem >>
+    let itemText = document.createTextNode(listItem);
+    // inject nodes into html >>
+    newLi.appendChild(itemText);
+    document.querySelector(".to-do-list").appendChild(newLi);
+    // clear input area for next item to add >>
+    document.querySelector("#new-to-do-item").value = "";
+  } else {
+    // remind user of input >>
+    document.querySelector("#new-to-do-item").placeholder =
+      "Please enter text first";
+  }
+};
